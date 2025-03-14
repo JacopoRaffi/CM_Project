379bfd76fbf75203295201997a6b0b97f8a36386
JacopoRaffi <raffijacopo@gmail.com>
2025-03-14 16:07:05 +0100

    preliminary implemented forward and bacward

--- src/math_utils.py
+++ src/math_utils.py
@@ -32,7 +32,16 @@ def forwad_substitution(A:np.ndarray, b:np.ndarray) -> np.ndarray:
     np.ndarray
         Solution vector
     '''
-    pass
+
+    w = np.zeros(b.shape)
+
+    for i in range(len(b)):
+        if A[i,i] == 0: #TODO: come risolvere?
+            raise ValueError("The diagonal element is zero")
+        
+        w[i] = (b[i] - np.dot(A[i, :i], w[:i])) / A[i, i]
+
+    return w
 
 def backward_substitution(A:np.ndarray, b:np.ndarray) -> np.ndarray:
     '''
@@ -50,7 +59,16 @@ def backward_substitution(A:np.ndarray, b:np.ndarray) -> np.ndarray:
     np.ndarray
         Solution vector
     '''
-    pass
+    
+    w = np.zeros(b.shape)
+
+    for i in range(len(b)-1, -1, -1):
+        if A[i,i] == 0: #TODO: come risolvere?
+            raise ValueError("The diagonal element is zero")
+        
+        w[i] = (b[i] - np.dot(A[i, i+1:], w[i+1:])) / A[i, i]
+    
+    return w
 
 def incr_QR(X_new:np.ndarray, householders:list, R:np.ndarray) -> tuple:
     '''
