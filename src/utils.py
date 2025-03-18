a58e7e267947e9e8c17482a7a8b9ce82bb45d4c6
JacopoRaffi <raffijacopo@gmail.com>
2025-03-18 17:03:57 +0100

    rename file

---
+++ src/utils.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+def mae(y_true, y_pred):
+    '''
+    Mean Absolute Error (MAE) metric
+
+    Parameters:
+    -----------
+    y_true: np.ndarray
+        True labels
+    y_pred: np.ndarray
+        Predicted labels
+
+    Returns:
+    --------
+    float
+    '''
+    return np.mean(np.abs(y_true - y_pred))
+
+def mse(y_true, y_pred):
+    '''
+    Mean Squared Error (MSE) metric
+
+    Parameters:
+    -----------
+    y_true: np.ndarray
+        True labels
+    y_pred: np.ndarray
+        Predicted labels
+    
+    Returns:
+    --------
+    float
+    '''
+    return np.mean((y_true - y_pred) ** 2)
